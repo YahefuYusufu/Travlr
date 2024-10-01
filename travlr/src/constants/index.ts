8f33a8e3ce90d3a75873b238df99004c634f06e1
YahefuYusufu <parguzatt@gmail.com>
2024-10-01 12:25:48 +0200

    Categories has done

---
+++ travlr/src/constants/index.ts
@@ -0,0 +1,30 @@
+export const categoriesData = [
+	{
+		title: "Ocean",
+		image: require("../../assets/images/cards/cards-01.png"),
+	},
+	{
+		title: "Mountain",
+		image: require("../../assets/images/cards/cards-02.png"),
+	},
+	{
+		title: "Camp",
+		image: require("../../assets/images/cards/cards-03.png"),
+	},
+	{
+		title: "Sunset",
+		image: require("../../assets/images/cards/cards-04.png"),
+	},
+	{
+		title: "Hiking",
+		image: require("../../assets/images/cards/cards-05.png"),
+	},
+	{
+		title: "Beach",
+		image: require("../../assets/images/cards/cards-06.png"),
+	},
+	{
+		title: "Forest",
+		image: require("../../assets/images/cards/cards-01.png"),
+	},
+]
